67007a8300f1d4fff6999a7646a64571459c65c7
James <j.avery@ucl.ac.uk>
2017-10-03 15:26:56 +0100

    Frequencies on Anna Hospital PC

--- src/DDS_PCB/frequencies.h
+++ src/DDS_PCB/frequencies.h
@@ -5,12 +5,12 @@
 
 #define NUM_CHANNELS 8
 
-#define FREQ_1 1000
-#define FREQ_2 2000
-#define FREQ_3 3000
-#define FREQ_4 4000
-#define FREQ_5 5000
-#define FREQ_6 6000
+#define FREQ_1 5000
+#define FREQ_2 6000
+#define FREQ_3 7000
+#define FREQ_4 8000
+#define FREQ_5 9000
+#define FREQ_6 10000
 #define FREQ_7 7000
 #define FREQ_8 8000
 
